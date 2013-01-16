74105e833f923fc5b59e585f31bac76449ea741e
saadettin <saadettin@saadettin-Pegatron.(none)>
2013-01-16 11:09:10 +0200

    scaffold_validation commiti

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Addrecord::Application.config.secret_token = 'd09fa6e594759a754f2716ae76717c21750defb5a0a05e45fca039d84305d38ab169ac5bd91b8784180a2d01cd87484109e71bd871f8bd0cf6c67120a730ec24'
